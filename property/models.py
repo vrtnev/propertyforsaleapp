3467c0087631c3afafa933389aa415d102fed52d
Nikolay Vorotnev <vrtnev@tltdev.ru>
2021-01-17 17:37:07 +0300

    Some fixes

--- property/models.py
+++ property/models.py
@@ -11,5 +11,6 @@ class Property(models.Model):
     ownerContact = models.CharField(max_length=256)
     sold = models.BooleanField(default=False)
 
+
     def __str__(self):
         return self.name
