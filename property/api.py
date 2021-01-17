e88efa4f2825907da8e9bedd41944e4633f46230
Nikolay Vorotnev <vrtnev@tltdev.ru>
2021-01-17 17:36:12 +0300

    Create database model

--- property/api.py
+++ property/api.py
@@ -2,6 +2,7 @@ from .models import Property
 from rest_framework import viewsets, permissions
 from .serializers import PropertySerializer
 
+
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     permissions_classes = [
